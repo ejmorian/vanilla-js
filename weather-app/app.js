c24618d08b987aa2ba4c3e172a5712eb71f2c146
jmorian <117075264+ejmorian@users.noreply.github.com>
2023-03-18 22:41:46 +0800

    Fixed Functionality
    + Allow users to search for weather conditions in a specific location,
    + Display additional weather data such as humidity, wind speed, and the chance of precipitation

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,32 +1,43 @@
-const getUserLocation = async () => {
-  "use strict";
+const displayWeather = async () => {
+  const { city, temperature, description, humidity, wind } = await getWeather();
 
-  const getLocation = await new Promise((resolve, reject) => {
-    navigator.geolocation.getCurrentPosition(
-      (position) => {
-        const userLatitude = position.coords.latitude;
-        const userLongitude = position.coords.longitude;
-        resolve({ userLatitude, userLongitude });
-      },
-      (error) => {
-        console.log(error);
-        reject(error);
-      }
-    );
-  });
-  return getLocation;
+  document.getElementById("city").textContent = `City: ${city}`;
+  document.getElementById("temperature").textContent = `temperature: ${(
+    temperature - 273.15
+  ).toFixed()}°`;
+  document.getElementById(
+    "description"
+  ).textContent = `description: ${description}`;
+  document.getElementById("humidity").textContent = `humidity: ${humidity}%`;
+  document.getElementById("wind").textContent = `wind: ${wind}m/s`;
+
+  console.log(city, temperature, description, humidity, wind);
 };
 
 const getWeather = () => {
-  "use strict";
+  const APIKEY = "a2665728a2c7b60b06b2fc309b36a104";
+  const CITYNAME = document.getElementById("city-input").value;
+
+  return fetch(
+    `https://api.openweathermap.org/data/2.5/weather?q=${CITYNAME}&appid=${APIKEY}`
+  )
+    .then((response) => response.json())
+    .then((weather) => {
+      console.log(weather);
+      const city = weather.name;
+      const temperature = weather.main.temp;
+      const description = weather.weather[0].description;
+      const humidity = weather.main.humidity;
+      const wind = weather.wind.speed;
+
+      return { city, temperature, description, humidity, wind };
+    });
 };
 
 const init = () => {
-  "use strict";
+  const searchButton = document.getElementById("search-button");
 
-  getUserLocation().then((result) => {
-    console.log(result);
-  });
+  searchButton.addEventListener("click", displayWeather);
 };
 
 window.onload = init;
