f108009d99b2a23755eee51877124539835c35aa
ejmorian <ejmorian@gmail.com>
2023-04-02 22:50:15 +0800

    add note-taking project

---
+++ typewriter/js/app.js
@@ -0,0 +1,15 @@
+const word = document.getElementById('hello')
+console.log(word.length)
+
+for (let i = 0; i <= word.innerText.length; i++) {
+    setTimeout(() => { console.log[i] }, 1000)
+
+
+}
+
+
+
+
+
+
+
