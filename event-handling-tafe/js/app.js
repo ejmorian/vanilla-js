27ad3f1348451bcf7713b050d375e5e67c5469cd
ejmorian <ejmorian@gmail.com>
2023-04-03 19:34:29 +0800

    Fix mispelled green box bug

--- event-handling-tafe/js/app.js
+++ event-handling-tafe/js/app.js
@@ -1,17 +1,19 @@
+/* jshint esversion: 9 */
 // get references to the buttons and boxes
-
 const buttons = document.querySelectorAll('button');
-const [greenBox, redBox, blueBox] = document.querySelectorAll('#green, #red, #blueIn')
+const [greenBox, redBox, blueBox] = document.querySelectorAll('#green, #red, #blueIn');
 
 // remove the hidden class and reset positions
 function show() {
+
     redBox.classList.remove('hidden');
     blueBox.classList.remove('hidden');
-    green.classList.remove('hidden');
+    greenBox.classList.remove('hidden');
 }
 
 // move boxes using the buttons 
 function utilities() {
+
     buttons.forEach((button, index) => {
         // blue box translations
         const translations = ['0 0', '0 600px', '600px 0', '-320px 0', '45px 20px', '0,0'];
@@ -21,9 +23,10 @@ function utilities() {
             redBox.style.translate = '0 0';
             if (index === 3) redBox.style.translate = '320px 0';
             if (index === 5) greenBox.classList.toggle('bgGreen');
+
             show();
-        })
-    })
+        });
+    });
 }
 
 window.onload = utilities;
